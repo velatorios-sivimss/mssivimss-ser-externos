dbab8181c634c13c41d2e445a8ecce78a57aeb58
LeandroPeopleMedia <107154425+LeandroPeopleMedia@users.noreply.github.com>
2023-04-21 10:39:54 -0600

    cambio requestMapping

--- src/main/java/com/imss/sivimss/serviciosexternos/controller/PeticionesController.java
+++ src/main/java/com/imss/sivimss/serviciosexternos/controller/PeticionesController.java
@@ -26,7 +26,7 @@ import io.github.resilience4j.retry.annotation.Retry;
 import io.github.resilience4j.timelimiter.annotation.TimeLimiter;
 
 @RestController
-@RequestMapping("/sivimss/catalogos/externos")
+@RequestMapping("/catalogos/externos") 
 public class PeticionesController {
 
 	private static final org.slf4j.Logger log = org.slf4j.LoggerFactory.getLogger(PeticionesController.class);
