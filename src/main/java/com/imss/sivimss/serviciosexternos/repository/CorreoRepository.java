5e7af1241e83c452f3a0622ed9fd7341b8e29346
oliverPimentelL <oliver.pimentel@people-media.com.mx>
2023-05-25 19:00:33 -0600

    Se modifica por cambios en la BD

--- src/main/java/com/imss/sivimss/serviciosexternos/repository/CorreoRepository.java
+++ src/main/java/com/imss/sivimss/serviciosexternos/repository/CorreoRepository.java
@@ -10,9 +10,9 @@ import com.imss.sivimss.serviciosexternos.model.entity.CorreoEntity;
 
 public interface CorreoRepository extends JpaRepository<CorreoEntity, Integer> {
 
-	@Query(value = "SELECT stc.ASUNTO AS asunto,stc.CUERPO_CORREO AS cuerpoCorreo "
+	@Query(value = " SELECT stc.DES_ASUNTO AS asunto,stc.DES_CUERPO_CORREO AS cuerpoCorreo "
 			+ " FROM svc_tipo_correo stc "
-			+ " WHERE stc.TIPO_CORREO = ?1 "
+			+ " WHERE stc.DES_TIPO_CORREO = ?1 "
 			, nativeQuery = true)
 	Optional<CorreoResponse> buscarCuerpoCorreo(String tipoCorreo);
 
